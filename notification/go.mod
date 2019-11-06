dd674be9a4d0ac84b6b2e133cb0b4ccb4eebabaa
xpFelix <1092702994@qq.com>
2019-11-06 20:00:18 +0800

    消息订阅

---
+++ notification/go.mod
@@ -0,0 +1,24 @@
+module shopping/notification
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/micro/go-log v0.1.0
+	github.com/micro/go-micro v1.14.0
+	github.com/micro/go-plugins v1.1.0
+	product v0.0.0
+	user v0.0.0
+)
+
+replace (
+	product => ../product
+	user => ../user
+)
+
+replace (
+	github.com/golang/lint => golang.org/x/lint v0.0.0-20190409202823-959b441ac422
+	github.com/hashicorp/consul v1.4.3 => github.com/hashicorp/consul v1.4.3
+	github.com/micro/go-micro => github.com/micro/go-micro v1.1.0
+	github.com/testcontainers/testcontainer-go => github.com/testcontainers/testcontainers-go v0.0.3
+)
---
+++ notification/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/micro/go-log"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/broker"
+	"github.com/micro/go-plugins/broker/rabbitmq"
+	"shopping/notification/subscriber"
+)
+
+func main() {
+
+	b := rabbitmq.NewBroker(
+		broker.Addrs("amqp://guest:guest@127.0.0.1:5672"),
+	)
+
+	b.Init()
+	b.Connect()
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.notification"),
+		micro.Version("latest"),
+		micro.Broker(b),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	//notification.RegisterNotificationHandler(service.Server(), new(handler.Notification))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("notification.submit", service.Server(), new(subscriber.Notification))
+
+	// Register Function as Subscriber
+	//micro.RegisterSubscriber("go.micro.srv.notification", service.Server(), subscriber.Handler)
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
